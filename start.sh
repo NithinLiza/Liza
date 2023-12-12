634e839b0f0d856a661c9a653dd5a38e52dc18bf
Lizamwol <85516053+NithinLiza@users.noreply.github.com>
2023-12-12 10:53:43 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Moviezclubhouse/Ramanan.git /Ramanan
+  git clone https://github.com/Moviezclubhouse/Liza.git /Liza
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ramanan
+  git clone $UPSTREAM_REPO /Liza
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
-echo "Starting Ramanan...."
+echo "Starting Liza...."
 python3 bot.py
